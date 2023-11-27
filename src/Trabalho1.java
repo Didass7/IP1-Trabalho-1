a94357ebb4315b73386dc516a9b46e8de61ec1f6
didas <diogon.oliveira@hotmail.com>
2023-11-27 20:06:00 +0000

    yau

---
+++ src/Trabalho1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Trabalho1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int tamMax=100;
+        String [] nome = new String[tamMax];
+        double [] quanto = new double[tamMax];
+        double [] preco = new double[tamMax];
+        boolean [] feito = new boolean[tamMax];
+        int nItens=0;
+
+        while (true) {
+            System.out.println("MENU\n(E)ditar lista.\n(F)azer compras.\nFazer (C)ontas.\n(S)air");
+            String op = scanner.next();
+
+
+        }
+    }
+}
