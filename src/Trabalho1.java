2e508ddb80db8b12c2127d77756fb6ae9d55fb67
BuildTools <unconfigured@null.spigotmc.org>
2023-12-02 00:49:37 +0000

    yau2

--- src/Trabalho1.java
+++ src/Trabalho1.java
@@ -1,22 +1,106 @@
 import java.util.Scanner;
 
 public class Trabalho1 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int tamMax=100;
-        String [] nome = new String[tamMax];
-        double [] quanto = new double[tamMax];
-        double [] preco = new double[tamMax];
-        boolean [] feito = new boolean[tamMax];
-        int nItens=0;
+        int tamMax = 100;
+        String[] nome = new String[tamMax];
+        double[] quanto = new double[tamMax];
+        double[] preco = new double[tamMax];
+        boolean[] feito = new boolean[tamMax];
+        int nItens = 0;
 
-        while (true) {
-            System.out.println("MENU\n(E)ditar lista.\n(F)azer compras.\nFazer (C)ontas.\n(S)air");
-            String op = scanner.next();
+        String menuPrincipal = "(E)ditar lista.\n"
+                + "(F)azer compras.\n"
+                + "Fazer (c)ontas.\n"
+                + "(S)air";
 
+        String editarLista = "----------------------------------------\n"
+                + "(I)nserir item no fim da lista.\n"
+                + "Inserir item na (p)osição n da lista.\n"
+                + "(A)pagar último item inserido na lista.\n"
+                + "Apagar item na posição (n) da lista.\n"
+                + "(a)pagar itens da posição m à n da lista.\n"
+                + "(L)istar todos os itens.\n"
+                + "(V)oltar.\n"
+                + "----------------------------------------\n";
 
+        String fazerCompras =  "----------------------------------------\n"
+                + "(M)arcar primeiro item por comprar.\n"
+                + "(D)esmarcar primeiro item comprado.\n"
+                + "Trocar estado por (n)ome.\n"
+                + "Trocar estado por (p)osição.\n"
+                + "(L)istar.\n"
+                + "(V)oltar.\n"
+                + "----------------------------------------\n";
+
+        String listar = "----------------------------------------\n"
+                + "Listar (t)odos os itens.\n"
+                + "Listar itens (c)omprados.\n"
+                + "Listar itens (p)or comprar.\n"
+                + "----------------------------------------\n";
+
+        String fazerContas = "Quanto custa a (l)ista?\n"
+                + "Quanto já (g)astei?\n"
+                + "Quanto custa o que (f)alta comprar?\n"
+                + "Qual o preço (m)édio por item?"
+                + "----------------------------------------\n";
+
+        char op;
+
+        //////////////////////////////////////
+        ////////////MENU PRINCIPAL////////////
+        //////////////////////////////////////
+
+        do {
+            System.out.println(menuPrincipal);
+            op = scanner.next().charAt(0);
+            switch (op) {
+                case 'E':
+                    System.out.println(editarLista);
+                    break;
+                case 'F':
+                    System.out.println(fazerCompras);
+                    break;
+                case 'c':
+                    System.out.println(fazerContas);
+                    break;
+                default:
+                    System.out.println("Opção Inválida.");
+            }
+        }
+        while (op!='S');{
+            System.out.println("Saiu do Programa ");
+        }
+
+        //////////////////////////////////////
+        //////// MENU (E)DITAR LISTA /////////
+        //////////////////////////////////////
+        switch (op) {
+            case 'I':
+                System.out.println(editarLista);
+                break;
+            case 'p':
+                System.out.println(fazerCompras);
+                break;
+            case 'A':
+                System.out.println(fazerContas);
+                break;
+            case 'n':
+                System.out.println(editarLista);
+                break;
+            case 'a':
+                System.out.println(fazerCompras);
+                break;
+            case 'L':
+                System.out.println(fazerContas);
+                break;
+            case 'V':
+                return;
+            default:
+                System.out.println("Opção Inválida.");
         }
     }
 }
